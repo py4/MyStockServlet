24fadba6845b825f6bf0bb4d13d3bdf38a0ebbdf
ehsan hajyasini <ehsanhajyasini74@gmail.com>
2016-05-27 21:38:43 +0430

    admin can view other users profiles

---
+++ src/ir/Epy/MyStock/controllers/AdminUserList.java
@@ -0,0 +1,41 @@
+package ir.Epy.MyStock.controllers;
+
+import ir.Epy.MyStock.Constants;
+import ir.Epy.MyStock.DAOs.CustomerDAO;
+import ir.Epy.MyStock.models.Customer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * Created customer_id esihaj on 4/8/16.
+ */
+@WebServlet("/admin/users")
+public class AdminUserList extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //customer, share_list, sell_queue, buy_queue
+
+        ArrayList<String> errors = new ArrayList<>();
+
+        try {
+            request.setAttribute("user_list", CustomerDAO.I().getAll());
+        } catch (SQLException e) {
+            e.printStackTrace();
+            errors.add(Constants.SQLExceptionMessage);
+        }
+        if (errors.size() > 0)
+            request.setAttribute("errors", errors);
+        request.getRequestDispatcher("/admin/view_user.jsp").forward(request, response);
+    }
+}
--- src/ir/Epy/MyStock/controllers/UserProfile.java
+++ src/ir/Epy/MyStock/controllers/UserProfile.java
@@ -4,6 +4,7 @@ import ir.Epy.MyStock.Constants;
 import ir.Epy.MyStock.DAOs.CustomerDAO;
 import ir.Epy.MyStock.DAOs.StockDAO;
 import ir.Epy.MyStock.DAOs.StockShareDAO;
+import ir.Epy.MyStock.exceptions.CustomerNotFoundException;
 import ir.Epy.MyStock.models.Customer;
 
 import javax.servlet.ServletException;
@@ -22,6 +23,29 @@ import java.util.ArrayList;
 public class UserProfile extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ArrayList<String> errors = new ArrayList<>();
+
+        String id = request.getParameter("user_id");
+        if(id == null || id.equals(""))
+            errors.add(Constants.NotProvided("user_id"));
+
+        try {
+            Customer c = CustomerDAO.I().find(id);
+            request.setAttribute("customer", c);
+            if (c.is_customer()) {
+                request.setAttribute("share_list", c.getShares());
+                request.setAttribute("request_list", c.getRequests());
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            errors.add(Constants.SQLExceptionMessage);
+        } catch (CustomerNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (errors.size() > 0)
+            request.setAttribute("errors", errors);
+        request.getRequestDispatcher("/customers/profile.jsp").forward(request, response);
         doGet(request, response);
     }
 
